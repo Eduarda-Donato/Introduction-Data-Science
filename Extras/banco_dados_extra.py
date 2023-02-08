ee9856e30c576d814aafdfc768934a5d05ab97c7
Maria Eduarda Donato <maria.donato@academico.ufpb.br>
2023-02-07 21:59:22 -0300

    updating studies

---
+++ Extras/banco_dados_extra.py
@@ -0,0 +1,6 @@
+import pandas as pd
+alunos = pd.DataFrame({'Nome': ['Ary', 'Cátia', 'Denis', 'Beto', 'Bruna', 'Dara', 'Carlos', 'Alice'], 
+                        'Sexo': ['M', 'F', 'M', 'M', 'F', 'F', 'M', 'F'], 
+                        'Idade': [15, 27, 56, 32, 42, 21, 19, 35], 
+                        'Notas': [7.5, 2.5, 5.0, 10, 8.2, 7, 6, 5.6]}, 
+                        columns = ['Nome', 'Idade', 'Sexo', 'Notas'])
